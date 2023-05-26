d6cdb2d6184d1193dcb7f073d8b330a2a94f2b1b
Alper Gezer <a.yz07x@gmail.com>
2023-05-26 23:03:26 +0300

    documentated so i can understand later

--- passport-config.js
+++ passport-config.js
@@ -1,18 +1,18 @@
-const { authenticate } = require("passport")
+//requirements--------------------------------------------
 const bcrypt = require("bcrypt")
-
 const LocalStrategy = require("passport-local").Strategy
+//requirements--------------------------------------------
 
-function initialize(passport,getUserByEmail,getUserById){
+function initialize(passport,getUserByEmail,getUserById){ //we give the passport module as a paremeter for config,and 2 functions for your database querries so it is modifyable
     const authenticateUser = async (email,password,done) =>{
-        const user = getUserByEmail(email)
+        const user = getUserByEmail(email)//the function you have given that querries database
         if(user==null){
-            return done(null,false,{message:"invalid email"})
+            return done(null,false,{message:"invalid email"}) //if user is not found,it returns a done function with message
         }
-        try {
-            if(await bcrypt.compare(password,user.password)){
-                return done(null,user)
-            }else{
+        try {   //try catch is used to catch any possible errors for auth
+            if(await bcrypt.compare(password,user.password)){   //the inputted pasword is being hashed, then compared to querried hash
+                return done(null,user)  //returns done function with the user that is going to be authenticated
+            }else{  //returns if password doesnt match
                 return done(null,false,{message:"invalid password"})
             }
         } catch (error) {
@@ -20,10 +20,9 @@ function initialize(passport,getUserByEmail,getUserById){
         }
     }
     passport.use(new LocalStrategy({usernameField:"email"},authenticateUser))
-    passport.serializeUser((user,done)=>done(null,user.id))
-    passport.deserializeUser((id,done)=>{
-        return done(null,getUserById(id))
-    })
+    //we define localstrategy, but because we use email instead of username,we have to specify it
+    passport.serializeUser((user,done)=>done(null,user.id))//it serializes user
+    passport.deserializeUser((id,done)=>{return done(null,getUserById(id))})//it deserializes user
 }
 
-module.exports = initialize
+module.exports = initialize // we export the main function to config our passaport
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 if (process.env.NODE_ENV !=="production"){
     require("dotenv").config()
 }
-
+//requirements------------------------------------
 const express = require("express")
 const bcrypt = require("bcrypt")
 const Pconfig = require("./passport-config")
 const passport = require("passport")
 const flash = require("express-flash")
 const session = require("express-session")
+//requirements-----------------------------------
 
-Pconfig(passport,email=>users.find(user =>user.email===email),id=>users.find(user =>user.id===id))
+//special function for passaport config, and localstrategy
+Pconfig(passport,email=>users.find(user =>user.email===email),id=>users.find(user =>user.id===id))//passaport module is given with inline 2 functions
 
+//this is temporary database
 const users=[]
 
 const app = express()
-app.set("view-engine","ejs")
-app.use(express.urlencoded({extended:true}))
-app.use(flash())
+app.set("view-engine","ejs")//template engin installation
+app.use(express.urlencoded({extended:true}))//to parse post data
+app.use(flash())//for error messages if the password is incorrect etc.
+
+//session is being defined and given required parameters--------------
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave:false,
@@ -24,12 +29,14 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+//session params end------------------------------
 
+//index page that only auth users can acces, cAuth function checks it
 app.get("/",cAuth,(req,res)=>{
     res.render("index.ejs",{name:req.user.name})
 })
 
-//Login page
+//Login page-----------------------------------------
 app.get("/login",cNAuth,(req,res)=>{
     res.render("login.ejs")
 })
@@ -37,51 +44,60 @@ app.post("/login", passport.authenticate("local",{
     successRedirect:"/",
     failureRedirect:"/login",
     failureFlash:true
+    //passaport does the auth for us,takes username and password,compares it. we changed username to email in config file 
+    //and hashed it so it works. if succes or failure, associated things happen in params
 }))
+//login page-----------------------------------------
 
-//Register page
+//Register page--------------------------------------
 app.get("/register",cNAuth,(req,res)=>{
     res.render("register.ejs")
 })
 app.post("/register",async (req,res)=>{
     try {
         const Hpassword = await bcrypt.hash(req.body.password,10)
+        //we hash the incoming password
         users.push({
             id:Date.now().toString(),
             name : req.body.name,
             email:req.body.email,
             password:Hpassword
         })
+        //the inputs are being wroten to database
         res.redirect("/login")
     } catch (error) {
         console.log(error)
         res.redirect("/register")
     }
-    console.log(users)
 })
-//logout
+//register page---------------------------------------
+
+//logout----------------------------------------------
 app.post("/logout",(req,res)=>{
-    req.logout(function(err) {
+    req.logout(function(err) {  //we use logout function provided by passport
         if (err) { return next(err); }
         res.redirect('/login');
     });
 })
+//logout---------------------------------------------
 
+//custom functions for checking auth-------------------
 function cAuth(req,res,next){
     if(req.isAuthenticated()){
-        return next()
+        return next() //if user is authenticated, the next function in line is executed which is render function
     }
-    res.redirect("/login")
+    res.redirect("/login")//if not, user is redirected
 }
 
 function cNAuth(req,res,next){
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated()){  //this function is the same function above,just reverse
         res.redirect("/")
     }else{
         next()
     }
     
 }
+//custom functions for checking auth-------------------
 
 app.listen("3000",()=> {
     console.log("3000 postu dinlemede")
